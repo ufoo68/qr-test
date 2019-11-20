2d1ef42e950d88d9c13bdf194b2a420d551b684e
ufoo68 <mitarou.yuutarou@gmail.com>
2019-11-20 22:33:02 +0900

    Create react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
